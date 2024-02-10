7d30ffb2880532e80100711a5e295865590c6603
khan022 <sk.siam@outlook.com>
2024-02-10 19:40:22 +0600

    adding the base page with blue color v0.02

--- ptwop_app/ptwop_app/settings.py
+++ ptwop_app/ptwop_app/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ptwop_app.wsgi.application'
-
+ASGI_APPLICATION = 'ptwop_app.asgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
