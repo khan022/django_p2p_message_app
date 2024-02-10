a738dcefe6aaa34c90cbe6bd3a78ef9919898982
khan022 <sk.siam@outlook.com>
2024-02-10 21:42:04 +0600

    sign-up created using djangoauth v0.04

--- ptwop_app/ptwop/views.py
+++ ptwop_app/ptwop/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
+from .forms import *
+
 
 # Create your views here.
 def frontpage(request):
     return render(request, 'ptwop/frontpage.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+            
+            return redirect('frontpage')
+    
+    else:
+        form = SignUpForm()
+
+    return render(request, 'ptwop/signup.html', {'form' : form})
