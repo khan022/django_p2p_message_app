416eae96f457729380a9352c2be45dd3a82ec33c
khan022 <sk.siam@outlook.com>
2024-02-15 20:25:18 +0600

    message storing enabled. v0.12

--- room/consumers.py
+++ room/consumers.py
@@ -3,6 +3,10 @@ import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async, async_to_sync
 
+from django.contrib.auth.models import User
+
+from .models import Message, Room
+
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -32,6 +36,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         username = data['username']
         room = data['room']
 
+        #saving message to the database
+        await self.save_message(username, room, message)
+
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -57,5 +64,13 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'room': room
         })
         await self.send(text_data)
+
+    
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = Room.objects.get(slug=room)
+        
+        Message.objects.create(user=user, room=room, content=message)
 
         
--- room/views.py
+++ room/views.py
@@ -13,5 +13,6 @@ def rooms(request):
 @login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:25]
 
-    return render(request, 'room/room.html', {'room': room})
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
