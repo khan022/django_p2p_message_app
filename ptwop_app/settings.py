e7b0be98639b4c4a656c830bbea8c497021ebb35
khan022 <sk.siam@outlook.com>
2024-02-15 22:00:30 +0600

    trying a vercel deploymen v0.17

--- ptwop_app/settings.py
+++ ptwop_app/settings.py
@@ -95,15 +95,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        "TEST": {
-            "NAME": BASE_DIR / "test_db.sqlite3",
-        },
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'verceldb',
+        'USER': 'default',
+        'PASSWORD': 'o2BmM9CTsebd',
+        'HOST': 'ep-square-wind-a1isl5pk-pooler.ap-southeast-1.aws.neon.tech',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
